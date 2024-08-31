8605e5751a98ba19120961a6166e45221559072a
Mesery <meserygamer@gmail.com>
2024-08-31 16:28:38 +0300

    Add DI container into solution

--- ProjectManagementSystem.ConsoleApp/Startup.cs
+++ ProjectManagementSystem.ConsoleApp/Startup.cs
@@ -1,4 +1,9 @@
-﻿using ProjectManagementSystem.Core.DomainEntities;
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectManagementSystem.Application.Interfaces;
+using ProjectManagementSystem.Application.Services;
+using ProjectManagementSystem.Core.RepositoryInterfaces;
+using ProjectManagementSystem.Infrastructure.HashService;
+using ProjectManagementSystem.SQLiteDb.Repositories;
 
 namespace ProjectManagementSystem.ConsoleApp;
 
@@ -6,6 +11,11 @@ public static class Startup
 {
     public static void Main()
     {
-        Console.WriteLine("Hello world!");
+        ServiceCollection services = new ServiceCollection();
+        services.AddTransient<UserAuthenticator>();
+        services.AddTransient<UserAuthenticationService>();
+        services.AddSingleton<IPasswordHasher, PasswordHasher>();
+        services.AddTransient<IUserRepository, SQLiteUserRepository>();
+        ((UserAuthenticator?)services.BuildServiceProvider().GetService(typeof(UserAuthenticator)))?.AuthenticateUser();
     }
 }
